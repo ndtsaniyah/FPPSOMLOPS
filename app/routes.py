3560fe5b0e4a6d37ceca6537f881a133cdf3e90e
ndtsaniyah <90408425+ndtsaniyah@users.noreply.github.com>
2024-06-05 11:57:50 +0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from flask import request, jsonify, render_template
+from . import utils
 from flask import request, jsonify, render_template, current_app as app
 from model.labeling_model import label_data
 
@@ -8,5 +10,8 @@ def index():
 @app.route('/label', methods=['POST'])
 def label_data_route():
     data = request.json['data']
-    labeled_data = label_data(data)
+    labeled_data = utils.labeling_function(data)
     return jsonify(labeled_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
